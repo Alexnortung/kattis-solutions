356168cfd2a7d5529cb560b97a874c797ee6ba7b
Alexander Nortung <alex_nortung@live.dk>
2020-01-21 13:43:34 +0100

    moved all files for future testing and started heir's dilemma

---
+++ new-problem.sh
@@ -0,0 +1,6 @@
+
+
+NAME=$1
+
+mkdir -p "problems/$NAME/src"
+mkdir -p "problems/$NAME/testfiles"
---
+++ problems/heirs-dilemma/src/heirs-dilemma.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    let line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let num1 = line
+
+    Ok(());
+}
