f308fe18a224f8b100cb37c0232b8dc240b7c4fe
Alexander Nortung <alex_nortung@live.dk>
2021-10-05 12:25:27 +0200

    found runtime error and completed teque

--- problems/teque/src/teque.rs
+++ problems/teque/src/teque.rs
@@ -1,80 +1,52 @@
 use std::io;
 use std::collections::VecDeque;
 
-struct Teque {
-    startVec: VecDeque<usize>,
-    startVecLen: usize,
-    endVec: VecDeque<usize>,
-    endVecLen: usize,
+struct Teque<T> {
+    startVec: VecDeque<T>,
+    endVec: VecDeque<T>,
 }
 
-impl Teque {
-    fn new(capacity : usize) -> Teque {
+impl<T> Teque<T> {
+    fn new(capacity : usize) -> Teque<T> {
         Teque {
             startVec: VecDeque::with_capacity(capacity),
-            startVecLen: 0,
             endVec: VecDeque::with_capacity(capacity),
-            endVecLen: 0,
         }
     }
-    fn push_front(&mut self, value: usize) {
+    fn push_front(&mut self, value: T) {
         self.startVec.push_front(value);
-        self.startVecLen += 1;
     }
-    fn push_back(&mut self, value: usize) {
+    fn push_back(&mut self, value: T) {
         self.endVec.push_front(value);
-        self.endVecLen += 1;
     }
-    fn push_middle(&mut self, value: usize) {
+    fn push_middle(&mut self, value: T) {
         // balance the Vectors
-        let total_size = self.startVecLen + self.endVecLen;
-        let balance = (total_size + 1) / 2; // startVecLen needs to become this
-        let difference = self.startVecLen - balance; // positive means - move difference amount of elements to endVec
+        let total_size = self.startVec.len() + self.endVec.len();
+        let balance : isize = ((total_size as isize) + 1) / 2; // startVecLen needs to become this
+        let difference : isize = (self.startVec.len() as isize) - balance; // positive means - move difference amount of elements to endVec
         for _ in 0..difference {
-            self.startVecLen -= 1;
-            let moveValue = self.startVec[self.startVecLen];
-            if self.endVecLen == self.endVec.len() {
-                self.endVec.push_back(moveValue);
-            } else {
-                self.endVec[self.endVecLen] = moveValue;
-            }
-            self.endVecLen += 1;
+            let moveValue = self.startVec.pop_back().unwrap();
+            self.endVec.push_back(moveValue);
         }
         // move from endVec to startVec
         for _ in difference..0 {
-            self.endVecLen -= 1;
-            let moveValue = self.endVec[self.endVecLen];
-            if self.startVecLen == self.startVec.len() {
-                self.startVec.push_back(moveValue);
-            } else {
-                self.startVec[self.startVecLen] = moveValue;
-            }
-            self.startVecLen += 1;
+            let moveValue = self.endVec.pop_back().unwrap();
+            self.startVec.push_back(moveValue);
         }
 
         if total_size % 2 == 0 { // if it is an equal number we can just as well make it balanced
-            if self.startVecLen == self.startVec.len() {
-                self.startVec.push_back(value);
-            } else {
-                self.startVec[self.startVecLen] = value;
-            }
-            self.startVecLen += 1;
+            self.startVec.push_back(value);
         } else {
-            if self.endVecLen == self.endVec.len() {
-                self.endVec.push_back(value);
-            } else {
-                self.endVec[self.endVecLen] = value;
-            }
-            self.endVecLen += 1;
+            self.endVec.push_back(value);
         }
     }
 
-    fn get(&self, index: usize) -> usize {
-        if index < self.startVecLen {
-            return self.startVec[index];
+    fn get(&self, index: usize) -> &T {
+        if index < self.startVec.len() {
+            return self.startVec.get(index).unwrap();
         }
-        let endVecIndex = (self.endVecLen - 1) - (index - self.startVecLen);
-        return self.endVec[endVecIndex];
+        let endVecIndex = (self.endVec.len() - 1) - (index - self.startVec.len());
+        return self.endVec.get(endVecIndex).unwrap();
     }
 }
 
@@ -83,7 +55,7 @@ fn main() {
     io::stdin().read_line(&mut line)
         .expect("Could not read line");
     let num_lines = line.trim().parse::<usize>().unwrap();
-    let mut teque = Teque::new(num_lines);
+    let mut teque = Teque::<isize>::new(num_lines);
     for _ in 0..num_lines {
         let mut line = String::new();
         io::stdin().read_line(&mut line)
@@ -91,12 +63,12 @@ fn main() {
         let mut splitted_line = line.trim().split_whitespace();
         let method = splitted_line.next().unwrap();
         let num_string = splitted_line.next().unwrap();
-        let value = num_string.parse::<usize>().unwrap();
+        let value = num_string.parse::<isize>().unwrap();
         match method {
             "push_back" => teque.push_back(value),
             "push_front" => teque.push_front(value),
             "push_middle" => teque.push_middle(value),
-            "get" => println!("{}", teque.get(value)),
+            "get" => println!("{}", teque.get(value as usize)),
             _ => (),
         }
     }
