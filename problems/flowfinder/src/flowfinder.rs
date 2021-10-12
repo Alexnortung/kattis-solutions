9f1692f363ce29f2c38f9e921dfc07f56d11cf5a
Alexander Nortung <alex_nortung@live.dk>
2021-10-12 13:12:01 +0200

    flowfinder: adjustments and tests. still fails

--- problems/flowfinder/src/flowfinder.rs
+++ problems/flowfinder/src/flowfinder.rs
@@ -1,17 +1,16 @@
 use std::io;
-use std::ptr;
 
 struct Node {
     flow: usize,
-    children: Vec<Node>,
+    children: Vec<Box<Node>>,
     solved: bool,
 }
 
 impl Node {
     fn sum_children(&self) -> usize {
-        let mut accumulator = 0;
+        let mut accumulator: usize = 0;
         for child in &self.children {
-            accumulator += child.flow;
+            accumulator += child.flow as usize;
         }
         accumulator
     }
@@ -38,7 +37,11 @@ fn solve_tree(node: &mut Node) -> bool {
         (true, 1) => {
             let sum = node.sum_children();
             let mut zero_node = node.children.iter_mut().find(|child| child.flow == 0).unwrap();
-            zero_node.flow = node.flow - sum;
+            let zero_node_flow: isize = node.flow as isize - sum as isize;
+            if zero_node_flow <= 0 {
+                return false;
+            }
+            zero_node.flow = zero_node_flow as usize;
         },
         (false, 0) => {
             node.flow = node.sum_children();
@@ -47,13 +50,13 @@ fn solve_tree(node: &mut Node) -> bool {
         _ => (),
     }
 
-    let mut children_results = Vec::with_capacity(2);
+    let mut children_results = Vec::with_capacity(node.children.len());
     for mut child in &mut node.children {
         let solved = if child.solved { true } else { solve_tree(&mut child) };
         children_results.push(solved);
     }
     let num_children_solved = children_results.iter().fold(0, |acc, solved| acc + (*solved as usize));
-    let diff_children_solved = node.children.len() - num_children_solved;
+    let diff_children_solved: usize = node.children.len() - num_children_solved;
     match (diff_children_solved, has_value) {
         (0, false) => {
             node.flow = node.sum_children();
@@ -61,13 +64,20 @@ fn solve_tree(node: &mut Node) -> bool {
             return true;
         },
         (0, true) => {
+            if node.sum_children() != node.flow {
+                return false;
+            }
             node.solved = true;
             return true;
         },
         (1, true) => {
             let sum = node.sum_children();
+            let zero_node_flow: isize = node.flow as isize - sum as isize;
+            if zero_node_flow <= 0 {
+                return false;
+            }
             if let Some(mut zero_node) = node.children.iter_mut().find(|child| child.flow == 0) {
-                zero_node.flow = node.flow - sum;
+                zero_node.flow = zero_node_flow as usize;
                 let solved = solve_tree(&mut zero_node);
                 node.solved = solved;
                 return solved;
@@ -83,7 +93,7 @@ fn main() {
     let mut line = String::new();
     io::stdin().read_line(&mut line)
         .expect(".");
-    let n = line.trim().parse::<usize>().unwrap();
+    let n: usize = line.trim().parse::<usize>().unwrap();
     let mut line = String::new();
     io::stdin().read_line(&mut line)
         .expect(".");
@@ -92,19 +102,18 @@ fn main() {
     io::stdin().read_line(&mut line)
         .expect(".");
     let mut splitted_line_2 = line.trim().split_whitespace();
-    let mut root = Node {
+    let root = Node {
         flow: parse_next(&mut splitted_line_2),
         children: Vec::with_capacity(2),
         solved: false,
     };
-    //let mut nodes: Vec<*mut isize> = Vec::with_capacity(n);
-    //let mut nodes: Vec<&mut Node> = Vec::with_capacity(n);
-    //let mut nodes_missing: Vec<usize> = Vec::with_capacity(n);
     let mut nodes: Vec<*mut Node> = Vec::with_capacity(n);
-    nodes.push(ptr::addr_of_mut!(root));
+    let mut boxed_root = Box::new(root);
+    let root_pointer: *mut Node = &mut *boxed_root;
+    nodes.push(root_pointer);
     // setup all other nodes
     for _ in 1..n {
-        let _parent_index = parse_next(&mut splitted_line_1) - 1;
+        let _parent_index: usize = parse_next(&mut splitted_line_1) - 1;
         let parent = nodes[_parent_index];
         let flow = parse_next(&mut splitted_line_2);
         let node = Node {
@@ -112,14 +121,17 @@ fn main() {
             children: Vec::with_capacity(2),
             solved: false,
         };
+        let mut boxed_node = Box::new(node);
+        let node_pointer: *mut Node = &mut *boxed_node;
         unsafe {
-            (*parent).children.push(node);
-            let new_index = (*parent).children.len() - 1;
-            nodes.push(ptr::addr_of_mut!((*parent).children[new_index]));
+            (*parent).children.push(boxed_node);
+            //let new_index = (*parent).children.len() - 1;
+            //let node_pointer: *mut Node = &mut ((*parent).children[new_index]);
+            nodes.push(node_pointer);
         }
     }
 
-    if !solve_tree(&mut root) {
+    if !solve_tree(&mut boxed_root) {
         println!("impossible");
         return ();
     }
