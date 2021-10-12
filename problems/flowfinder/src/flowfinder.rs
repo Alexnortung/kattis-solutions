12a89385ab21b6bbaeffa481f1dc1e38eb5f3d17
Alexander Nortung <alex_nortung@live.dk>
2021-10-12 12:13:04 +0200

    flowfinder validates locally

--- problems/flowfinder/src/flowfinder.rs
+++ problems/flowfinder/src/flowfinder.rs
@@ -1,16 +1,82 @@
 use std::io;
+use std::ptr;
 
 struct Node {
-    parent: &Node,
     flow: usize,
-    children: Vec<&Node>,
+    children: Vec<Node>,
+    solved: bool,
+}
+
+impl Node {
+    fn sum_children(&self) -> usize {
+        let mut accumulator = 0;
+        for child in &self.children {
+            accumulator += child.flow;
+        }
+        accumulator
+    }
 }
 
 fn parse_next(iter: &mut std::str::SplitWhitespace) -> usize {
     return iter.next().unwrap().parse::<usize>().unwrap();
 }
 
-fn recursive_traverse(node: &mut Node) {
+fn solve_tree(node: &mut Node) -> bool {
+    if node.solved {
+        return true;
+    }
+    // does this node have a value?
+    let mut has_value = node.flow != 0;
+    let is_leaf = node.children.len() == 0;
+    if is_leaf {
+        node.solved = has_value;
+        return has_value;
+    }
+    let children_with_values = node.children.iter().fold(0, |acc, child| acc + (child.flow != 0) as usize);
+    let diff_children = node.children.len() - children_with_values;
+    match (has_value, diff_children) {
+        (true, 1) => {
+            let sum = node.sum_children();
+            let mut zero_node = node.children.iter_mut().find(|child| child.flow == 0).unwrap();
+            zero_node.flow = node.flow - sum;
+        },
+        (false, 0) => {
+            node.flow = node.sum_children();
+            has_value = true;
+        },
+        _ => (),
+    }
+
+    let mut children_results = Vec::with_capacity(2);
+    for mut child in &mut node.children {
+        let solved = if child.solved { true } else { solve_tree(&mut child) };
+        children_results.push(solved);
+    }
+    let num_children_solved = children_results.iter().fold(0, |acc, solved| acc + (*solved as usize));
+    let diff_children_solved = node.children.len() - num_children_solved;
+    match (diff_children_solved, has_value) {
+        (0, false) => {
+            node.flow = node.sum_children();
+            node.solved = true;
+            return true;
+        },
+        (0, true) => {
+            node.solved = true;
+            return true;
+        },
+        (1, true) => {
+            let sum = node.sum_children();
+            if let Some(mut zero_node) = node.children.iter_mut().find(|child| child.flow == 0) {
+                zero_node.flow = node.flow - sum;
+                let solved = solve_tree(&mut zero_node);
+                node.solved = solved;
+                return solved;
+            }
+            node.solved = false;
+            return false;
+        },
+        _ => return false,
+    }
 }
 
 fn main() {
@@ -26,44 +92,42 @@ fn main() {
     io::stdin().read_line(&mut line)
         .expect(".");
     let mut splitted_line_2 = line.trim().split_whitespace();
-    let root = Node {
-        parent: null,
+    let mut root = Node {
         flow: parse_next(&mut splitted_line_2),
+        children: Vec::with_capacity(2),
+        solved: false,
     };
-    let mut nodes: Vec<Node> = Vec::with_capacity(n);
-    let mut nodes_missing: Vec<usize> = Vec::with_capacity(n);
-    nodes.push(root);
+    //let mut nodes: Vec<*mut isize> = Vec::with_capacity(n);
+    //let mut nodes: Vec<&mut Node> = Vec::with_capacity(n);
+    //let mut nodes_missing: Vec<usize> = Vec::with_capacity(n);
+    let mut nodes: Vec<*mut Node> = Vec::with_capacity(n);
+    nodes.push(ptr::addr_of_mut!(root));
     // setup all other nodes
-    for i in 1..n {
-        let parent_index = parse_next(&mut splitted_line_1);
-        let parent = &nodes[parent_index];
+    for _ in 1..n {
+        let _parent_index = parse_next(&mut splitted_line_1) - 1;
+        let parent = nodes[_parent_index];
         let flow = parse_next(&mut splitted_line_2);
-        if flow <= 0 {
-            nodes_missing.push(i);
-        }
-        nodes.push(Node {
-            parent: parent,
+        let node = Node {
             flow: flow,
             children: Vec::with_capacity(2),
-        });
-        parent.children.push(&nodes.last().unwrap());
+            solved: false,
+        };
+        unsafe {
+            (*parent).children.push(node);
+            let new_index = (*parent).children.len() - 1;
+            nodes.push(ptr::addr_of_mut!((*parent).children[new_index]));
+        }
     }
 
-    for i in nodes_missing {
-        let node = nodes[i];
-        let has_value = node.flow > 0;
-        if has_value {
-            continue;
-        }
-        let is_root = node.parent == null;
-        if !is_root && node.parent.flow > 0 {
-            // check parent
-            let other_flow = node.parent.children.iter()
-                .map(|c| c.flow)
-                .sum();
-            let remaining = node.parent.flow - other_flow;
-            node.flow = remaining;
-            continue;
+    if !solve_tree(&mut root) {
+        println!("impossible");
+        return ();
+    }
+
+    unsafe {
+        for node in nodes {
+            let node = &*node;
+            println!("{}", node.flow);
         }
     }
 }
--- problems/teque/src/teque.rs
+++ problems/teque/src/teque.rs
@@ -1,65 +1,75 @@
 use std::io;
+use std::io::BufReader;
+use std::io::BufRead;
 use std::collections::VecDeque;
 
 struct Teque<T> {
-    startVec: VecDeque<T>,
-    endVec: VecDeque<T>,
+    start_vec: VecDeque<T>,
+    end_vec: VecDeque<T>,
 }
 
 impl<T> Teque<T> {
     fn new(capacity : usize) -> Teque<T> {
         Teque {
-            startVec: VecDeque::with_capacity(capacity),
-            endVec: VecDeque::with_capacity(capacity),
+            start_vec: VecDeque::with_capacity(capacity),
+            end_vec: VecDeque::with_capacity(capacity),
         }
     }
     fn push_front(&mut self, value: T) {
-        self.startVec.push_front(value);
+        self.start_vec.push_front(value);
     }
     fn push_back(&mut self, value: T) {
-        self.endVec.push_front(value);
+        self.end_vec.push_front(value);
     }
     fn push_middle(&mut self, value: T) {
         // balance the Vectors
-        let total_size = self.startVec.len() + self.endVec.len();
-        let balance : isize = ((total_size as isize) + 1) / 2; // startVecLen needs to become this
-        let difference : isize = (self.startVec.len() as isize) - balance; // positive means - move difference amount of elements to endVec
+        let total_size = self.start_vec.len() + self.end_vec.len();
+        let balance : isize = ((total_size as isize) + 1) / 2; // start_vec needs to become this
+        let difference : isize = (self.start_vec.len() as isize) - balance; // positive means - move difference amount of elements to end_vec
         for _ in 0..difference {
-            let moveValue = self.startVec.pop_back().unwrap();
-            self.endVec.push_back(moveValue);
+            let move_value = self.start_vec.pop_back().unwrap();
+            self.end_vec.push_back(move_value);
         }
-        // move from endVec to startVec
+        // move from end_vec to start_vec
         for _ in difference..0 {
-            let moveValue = self.endVec.pop_back().unwrap();
-            self.startVec.push_back(moveValue);
+            let move_value = self.end_vec.pop_back().unwrap();
+            self.start_vec.push_back(move_value);
         }
 
         if total_size % 2 == 0 { // if it is an equal number we can just as well make it balanced
-            self.startVec.push_back(value);
+            self.start_vec.push_back(value);
         } else {
-            self.endVec.push_back(value);
+            self.end_vec.push_back(value);
         }
     }
 
     fn get(&self, index: usize) -> &T {
-        if index < self.startVec.len() {
-            return self.startVec.get(index).unwrap();
+        if index < self.start_vec.len() {
+            return self.start_vec.get(index).unwrap();
         }
-        let endVecIndex = (self.endVec.len() - 1) - (index - self.startVec.len());
-        return self.endVec.get(endVecIndex).unwrap();
+        let end_vec_index = (self.end_vec.len() - 1) - (index - self.start_vec.len());
+        return self.end_vec.get(end_vec_index).unwrap();
     }
 }
 
 fn main() {
     let mut line = String::new();
-    io::stdin().read_line(&mut line)
-        .expect("Could not read line");
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin);
+    let _len = reader.read_line(&mut line);
     let num_lines = line.trim().parse::<usize>().unwrap();
     let mut teque = Teque::<isize>::new(num_lines);
+    //let mut lines_iter = reader.lines();
     for _ in 0..num_lines {
-        let mut line = String::new();
-        io::stdin().read_line(&mut line)
-            .expect("Could not read line");
+        //match lines_iter.next() {
+        //    Some(Err(_)) => break,
+        //    Some(Ok(line_unwrapped)) => {
+        //        line = line_unwrapped;
+        //    },
+        //    None => break,
+        //}
+        line = String::new();
+        let _len = reader.read_line(&mut line);
         let mut splitted_line = line.trim().split_whitespace();
         let method = splitted_line.next().unwrap();
         let num_string = splitted_line.next().unwrap();
